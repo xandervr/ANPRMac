017c11ab5f369f95849cc9d7250ef122b8c372e5
Xander Van Raemdonck <xandervanraemdonck@gmail.com>
2016-04-13 22:21:47 +0200

    Speed Up scanner

--- ALPRMAC/ViewController.swift
+++ ALPRMAC/ViewController.swift
@@ -8,12 +8,14 @@
 
 import Cocoa
 import AVFoundation
+import QuartzCore
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
     var captureSession : AVCaptureSession?
     var videoPreviewLayer : AVCaptureVideoPreviewLayer?
     let output = AVCaptureStillImageOutput()
+    let outputStream = AVCaptureVideoDataOutput()
     
     var busy : Bool = false
     var start : Bool = false
@@ -64,6 +66,37 @@ class ViewController: NSViewController {
         
     }
     
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            CVPixelBufferLockBaseAddress(imageBuffer, 0)
+            
+            let width = CVPixelBufferGetWidth(imageBuffer)
+            let height = CVPixelBufferGetHeight(imageBuffer)
+            let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+            let src = CVPixelBufferGetBaseAddress(imageBuffer)
+            
+            let image = CIImage(CVPixelBuffer: imageBuffer)
+            let context = CIContext(options: nil)
+            let myImage = context.createCGImage(image, fromRect: CGRectMake(0, 0, CGFloat(width), CGFloat(height)))
+            let finalImage = NSImage(CGImage: myImage, size: NSSize(width: width, height: height))
+            
+            if let data = finalImage.TIFFRepresentation {
+                CVPixelBufferUnlockBaseAddress(imageBuffer, 0)
+                
+                let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+                do {
+                    try data.writeToFile(documentsPath + "/stream.jpg", options: .DataWritingAtomic)
+                    system("/usr/local/Cellar/openalpr/2.2.0/bin/alpr -c eu -j " + documentsPath + "/stream.JPG > " + documentsPath + "/stream.txt")
+                    self.scanText("/stream")
+                } catch {
+                    print(error)
+                }
+                
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -72,6 +105,7 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
         
         let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
         var input : AVCaptureDeviceInput?
         do {
             input = try AVCaptureDeviceInput(device: captureDevice)
@@ -88,8 +122,11 @@ class ViewController: NSViewController {
         
         
         output.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+        outputStream.setSampleBufferDelegate(self, queue: dispatch_queue_create("SampleBuffer", DISPATCH_QUEUE_SERIAL))
         
         captureSession?.addOutput(output)
+        captureSession?.addOutput(outputStream)
+        
         if let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
             videoPreviewLayer.bounds = view.bounds
             videoPreviewLayer.position = CGPointMake(view.bounds.midX, view.bounds.midY)
@@ -111,6 +148,7 @@ class ViewController: NSViewController {
             button.bezelStyle = .RoundedBezelStyle
             button.target = self
             button.action = #selector(ViewController.buttonPressed)
+            button.hidden = true
             
             txtPlate.backgroundColor = NSColor.clearColor()
             txtPlate.textColor = NSColor.redColor()
@@ -167,6 +205,11 @@ class ViewController: NSViewController {
                     dispatch_async(dispatch_get_main_queue()) {
                         if let type : String = message["type"] as? String {
                             self.txtType.string = type
+                            if let correctPlate : String = message["text"] as? String {
+                                self.txtPlate.string = self.craftPlate(correctPlate).uppercaseString
+                            } else {
+                                self.txtPlate.string = self.craftPlate(plate).uppercaseString
+                            }
                         }
                     }
                     flagged = true
@@ -178,6 +221,27 @@ class ViewController: NSViewController {
         }
         
         return flagged
+    }
+    
+    func logPlate(plate : String) -> Bool {
+        var logged : Bool = false
+        var data = NSData()
+        var url = "https://api.tntap.be/?panpr&logPlate=" + plate
+        url += "&appVersion=2"
+        let endpoint = NSURL(string: url)
+        do {
+            try data = NSData(contentsOfURL: endpoint!, options: .DataReadingUncached)
+            if let message : NSDictionary = self.JSONParseDictionaryFromData(data) {
+                if message["message"] as? String == "success" {
+                    logged = true
+                }
+            }
+        } catch _ {
+            NSLog("[HTTP REQUEST]: Network error!")
+            self.logPlate(plate)
+        }
+        
+        return logged
     }
     
     func isRealPlate(plate : String) -> Bool {
@@ -239,62 +303,75 @@ class ViewController: NSViewController {
                     
                     imageData.writeToFile(documentsPath + "/output.JPG", atomically: true)
                     system("/usr/local/Cellar/openalpr/2.2.0/bin/alpr -c eu -j " + documentsPath + "/output.JPG > " + documentsPath + "/output.txt")
-                    do {
-                        let text2 = try String(contentsOfFile: documentsPath + "/output.txt")
-                        
-                        
-                        let info : NSDictionary = self.JSONParseDictionary(text2)
-                        if info.count > 0 {
-                            if let plateArray : [[String : AnyObject]] = info["results"] as? [[String : AnyObject]] {
-                                for item in plateArray {
-                                    if let plate : String = item["plate"] as? String {
-                                        print(plate)
-                                        if self.isRealPlate(plate) {
-                                            let length = NSString(string: plate).length
-                                            if (length == 6 || length == 7) {
-                                                var craftedPlate = plate
-                                                if length == 7 {
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(1))
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(5))
-                                                } else {
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(3))
-                                                }
-                                                dispatch_async(dispatch_get_main_queue()) {
-                                                    self.txtTryPlate.string = craftedPlate.uppercaseString
-                                                }
-                                            }
-                                            
-                                            
-                                            if (length == 6 || length == 7) && self.sendPlate(plate) {
-                                                var craftedPlate = plate
-                                                if length == 7 {
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(1))
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(5))
-                                                } else {
-                                                    craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(3))
-                                                }
-                                                dispatch_async(dispatch_get_main_queue()) {
-                                                    self.txtPlate.string = craftedPlate.uppercaseString
-                                                }
-                                                print("WARNING!!!!")
-                                                NSBeep()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        
-                        system("rm " + documentsPath +  "/output.JPG")
-                        system("rm " + documentsPath +  "/output.txt")
-                    } catch {
-                        print(error)
-                        
-                    }
+                    self.scanText("/output")
                 }
             })
         }
         busy = false
+    }
+    
+    func craftPlate(plate : String) -> String {
+        let length = NSString(string: plate).length
+        if (length == 6 || length == 7) {
+            var craftedPlate = plate
+            if length == 7 {
+                craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(1))
+                craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(5))
+            } else {
+                craftedPlate.insert("-", atIndex: craftedPlate.startIndex.advancedBy(3))
+            }
+            return craftedPlate
+        } else {
+            return ""
+        }
+    }
+    
+    func scanText(file : String) {
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        do {
+            let text2 = try String(contentsOfFile: documentsPath + file + ".txt")
+            
+            
+            let info : NSDictionary = self.JSONParseDictionary(text2)
+            if info.count > 0 {
+                if let plateArray : [[String : AnyObject]] = info["results"] as? [[String : AnyObject]] {
+                    print("----- NEW ------")
+                    var i : Int = 0
+                    for item in plateArray {
+                        i+=1
+                        print(i)
+                        if let plate : String = item["plate"] as? String {
+                            print(plate)
+                            if self.isRealPlate(plate) {
+                                
+                                self.logPlate(plate)
+                                
+                                let length = NSString(string: plate).length
+                                if (length == 6 || length == 7) {
+                                    dispatch_async(dispatch_get_main_queue()) {
+                                        self.txtTryPlate.string = self.craftPlate(plate).uppercaseString
+                                    }
+                                }
+                                
+                                
+                                if (length == 6 || length == 7) && self.sendPlate(plate) {
+                                    
+                                    
+                                    print("WARNING!!!!")
+                                    NSBeep()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            system("rm " + documentsPath + file + ".JPG")
+            system("rm " + documentsPath +  file + ".txt")
+        } catch {
+            print(error)
+            
+        }
     }
     
     override var representedObject: AnyObject? {
